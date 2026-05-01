874197f0c15a01322e55029ccef56fe7accd2933
Pritom-10 <pritomshiro@gmail.com>
2026-05-01 22:12:07 +0600

    signup and signin setup

--- src/lib/auth.js
+++ src/lib/auth.js
@@ -2,7 +2,7 @@ import { betterAuth } from "better-auth";
 import { MongoClient } from "mongodb";
 import { mongodbAdapter } from "better-auth/adapters/mongodb";
 
-const client = new MongoClient(process.env.MONGODB_UI);
+const client = new MongoClient(process.env.MONGODB_URI);
 const db = client.db("livestock");
 
 export const auth = betterAuth({
@@ -11,6 +11,6 @@ export const auth = betterAuth({
     client,
   }),
   emailAndPassword: {
-    enabled:true 
+    enabled: true,
   }
 });
